921bc95846617699617fcd7d19128d1b4be20c41
Koneist <mr.druzhinin.egor@gmail.com>
2022-05-17 12:09:45 +0300

    start lw5

---
+++ lw5/lw5/Figure.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace lw5
+{
+    enum CubeSide
+    {
+        NegativeX = 0,
+        PositiveX = 1,
+        NegativeY = 2,
+        PositiveY = 3,
+        NegativeZ = 4,
+        PositiveZ = 5,
+    }
+    internal class Figure
+    {
+        private static readonly float _fi = (1f + MathF.Sqrt(5)) / 2f;
+        private static Random _random = new(0);
+        private float _size;
+
+        private float[] _vertexBuffer;
+        private float[] _colorBuffer;
+        private float[] _normalBuffer;
+        private Vector4 _specularColor = new(0, 0, 0, 1);
+        private float _shininess = 1;
+
+        private List<Vector3> _verteces = new()
+        {
+            new(-1, -1, -1),          // 0
+            new(+1, -1, -1),          // 1
+            new(+1, +1, -1),          // 2
+            new(-1, +1, -1),          // 3
+            new(-1, -1, +1),          // 4
+            new(+1, -1, +1),          // 5
+            new(+1, +1, +1),          // 6
+            new(-1, +1, +1),          // 7
+            new(0, +_fi, +(1 / _fi)), // 8 
+            new(0, +_fi, -(1 / _fi)), // 9
+            new(0, -_fi, +(1 / _fi)), // 10
+            new(0, -_fi, -(1 / _fi)), // 11
+            new(+(1 / _fi), 0, +_fi), // 12 
+            new(+(1 / _fi), 0, -_fi), // 13
+            new(-(1 / _fi), 0, +_fi), // 14
+            new(-(1 / _fi), 0, -_fi), // 15
+            new(+_fi, +(1 / _fi), 0), // 16
+            new(+_fi, -(1 / _fi), 0), // 17
+            new(-_fi, +(1 / _fi), 0), // 18
+            new(-_fi, -(1 / _fi), 0)  // 19
+        };
+
+        private List<int[]> _faces = new()
+        {
+            new [] { 0, 15, 13, 1, 11 }, // 0
+            new [] { 0, 11, 10, 4, 19 }, // 1
+            new [] { 0, 19, 18, 3, 15 }, // 2
+            new [] { 1, 17, 5, 10, 11 }, // 3
+            new [] { 1, 13, 2, 16, 17 }, // 4
+            new [] { 2, 9, 8, 6, 16 }, // 7
+            new [] { 2, 13, 15, 3, 9 }, // 8
+            new [] { 3, 18, 7, 8, 9 }, // 11
+            new [] { 4, 14, 7, 18, 19 }, // 13
+            new [] { 4, 10, 5, 12, 14 }, // 14
+            new [] { 5, 17, 16, 6, 12 }, // 16
+            new [] { 6, 8, 7, 14, 12 }, // 20 
+        };
+
+        public Figure(float size = 1)
+        {
+            _size = size;
+
+            List<float> verteces = new();
+            List<float> normals = new();
+            List<float> colors = new();
+            foreach (var face in _faces)
+            {
+                var startVertex = face[0];
+                Color4 color = new((float)_random.NextDouble(), (float)_random.NextDouble(), (float)_random.NextDouble(), 1);
+                for (int i = 2; i < face.Length; ++i)
+                {
+                    AddVertex(verteces, _verteces[startVertex]);
+                    AddVertex(verteces, _verteces[face[i - 1]]);
+                    AddVertex(verteces, _verteces[face[i]]);
+
+                    var v0 = _verteces[face[i - 1]] - _verteces[startVertex];
+                    var v1 = _verteces[face[i]] - _verteces[startVertex];
+                    var normal = Vector3.Cross(v0, v1).Normalized();
+
+                    AddVertex(normals, normal);
+                    AddVertex(normals, normal);
+                    AddVertex(normals, normal);
+
+                    AddColor(colors, color);
+                    AddColor(colors, color);
+                    AddColor(colors, color);
+
+
+                }
+            }
+
+            _vertexBuffer = verteces.ToArray();
+            _normalBuffer = normals.ToArray();
+            _colorBuffer = colors.ToArray();
+        }
+
+        private Matrix4 _rotateMatrix = Matrix4.Identity;
+        public void Rotate(float angle, Vector3 axis)
+        {
+            _rotateMatrix *= Matrix4.CreateFromAxisAngle(axis, angle);
+        }
+        public void Draw()
+        {
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.ColorMaterial(MaterialFace.FrontAndBack, ColorMaterialParameter.AmbientAndDiffuse);
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Specular, _specularColor);
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Shininess, _shininess);
+
+            GL.PushMatrix();
+            GL.MultMatrix(ref _rotateMatrix);
+            GL.Scale(_size * 0.5f, _size * 0.5f, _size * 0.5f);
+
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.VertexPointer(3, VertexPointerType.Float, 0, _vertexBuffer);
+
+            GL.EnableClientState(ArrayCap.ColorArray);
+            GL.ColorPointer(4, ColorPointerType.Float, 0, _colorBuffer);
+
+            GL.EnableClientState(ArrayCap.NormalArray);
+            GL.NormalPointer(NormalPointerType.Float, 0, _normalBuffer);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, _vertexBuffer.Length / 3);
+
+            GL.DisableClientState(ArrayCap.VertexArray);
+            GL.DisableClientState(ArrayCap.ColorArray);
+            GL.DisableClientState(ArrayCap.NormalArray);
+
+            GL.PopMatrix();
+        }
+
+        private void AddVertex(List<float> buffer, Vector3 vertex)
+        {
+            buffer.Add(vertex.X);
+            buffer.Add(vertex.Y);
+            buffer.Add(vertex.Z);
+        }
+
+        private void AddColor(List<float> buffer, Color4 color)
+        {
+            buffer.Add(color.R);
+            buffer.Add(color.G);
+            buffer.Add(color.B);
+            buffer.Add(color.A);
+        }
+    }
+}
---
+++ lw5/lw5/Window.cs
@@ -0,0 +1,148 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using lw5.Object;
+
+namespace lw5
+{
+    internal class Window : GameWindow
+    {
+        //private Figure _figure = new Figure(1f);
+        private Planet _planet = new(@"E:\Projects\repository\SG\lw5\lw5\Texture\2k_earth_daymap.jpg");
+        private const float FIELD_OF_VIEW = 60 * MathF.PI / 180.0f;
+        // Размер стороны куба
+
+        private const float Z_NEAR = 0.1f;
+        private const float Z_FAR = 10;
+        private Matrix4 _cameraMatrix = Matrix4.LookAt(new(0, 0, 2),
+                                                       new(0, 0, 0),
+                                                       new(0, 2, 0));
+        private bool _leftMouseBtnPressed = false;
+
+        public static Window StartWindow(NativeWindowSettings nativeWindowSettings)
+        {
+            return new Window(GameWindowSettings.Default, nativeWindowSettings);
+        }
+
+        public Window(GameWindowSettings gameWindowSettings,
+                      NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            GL.Enable(EnableCap.Lighting);
+            GL.Enable(EnableCap.Light2);
+            GL.ClearColor(Color4.White);
+            GL.Enable(EnableCap.CullFace);
+            GL.CullFace(CullFaceMode.Back);
+            GL.FrontFace(FrontFaceDirection.Ccw);
+            GL.Enable(EnableCap.DepthTest);
+
+            DirectLight light = new(new(0, 0, 0));
+            light.SetDiffuseIntensity(new(0.5f, 0.5f, 0.5f, 1f));
+            light.SetAmbientIntensity(new(0.3f, 0.3f, 0.3f, 1.0f));
+            light.SetSpecularIntensity(new(1.0f, 1.0f, 1.0f, 1.0f));
+            light.Apply(LightName.Light2);
+
+            base.OnLoad();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+            _planet.Animate((float)e.Time);
+            
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            float aspect = (float)e.Width / (float)e.Height;
+            
+            GL.MatrixMode(MatrixMode.Projection);
+            var proj = Matrix4.CreatePerspectiveFieldOfView(FIELD_OF_VIEW, aspect, Z_NEAR, Z_FAR);
+
+            GL.LoadMatrix(ref proj);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+
+            base.OnResize(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            Draw();
+
+            SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButton.Button1)
+            {
+                _leftMouseBtnPressed = true;
+            }
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (e.Button == MouseButton.Button1)
+            {
+                _leftMouseBtnPressed = false;
+            }
+        }
+
+        protected override void OnMouseLeave()
+        {
+            base.OnMouseLeave();
+            _leftMouseBtnPressed = false;
+        }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (_leftMouseBtnPressed)
+            {
+                float xAngle = e.DeltaY * MathF.PI / Size.Y;
+                float yAngle = e.DeltaX * MathF.PI / Size.X;
+                RotateObject(xAngle, yAngle);
+            }
+        }
+
+        private void RotateObject(float xAngle, float yAngle)
+        {
+            Vector3 xAxis = new(_cameraMatrix.M11, _cameraMatrix.M21, _cameraMatrix.M31);
+            Vector3 yAxis = new(_cameraMatrix.M12, _cameraMatrix.M22, _cameraMatrix.M32);
+
+            //.Rotate(xAngle, xAxis);
+            //_figure.Rotate(yAngle, yAxis);
+        }
+
+        private void Draw()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref _cameraMatrix);
+
+            _planet.Draw();
+            //_figure.Draw();
+        }
+    }
+}
